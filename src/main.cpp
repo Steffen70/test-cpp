03bd318233bb595d3a32a5ab2e3da49fed00ea42
Steffen70 <steffen@seventy.mx>
2025-09-04 13:59:33 +0200

    Extended linear_search from the int array to also handle a Student array and a best-student-by-grade search.

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,11 @@ void* linear_search(void* keyPtr, void* arrPtr, size_t arraySize, size_t elemSiz
     return nullptr;
 }
 
-// int cmpFn(void* val1Ptr, void* val2Ptr)
-// {
-//     return memcmp(val1Ptr, val2Ptr, sizeof(int));
-// }
+struct Student
+{
+    double grade;
+    char* namePtr;
+};
 
 int main(int argc, char** argv)
 {
@@ -25,10 +26,39 @@ int main(int argc, char** argv)
     int key = 40;
 
     // void* elemAddrPtr = linear_search(&key, &intArr, 5, sizeof(int), +cmpFn);
-    void* elemAddrPtr = linear_search(&key, &intArr, 5, sizeof(int), +[](void* val1Ptr, void* val2Ptr) -> int {
+    void* elemAddrPtr = linear_search(&key, &intArr, 5, sizeof(int), +[](void* val1Ptr, void* val2Ptr) -> int
+    {
         return memcmp(val1Ptr, val2Ptr, sizeof(int));
     });
 
     fmt::println(stdout, "elemAddrPtr: {}, elemValue: {}, key: {}", fmt::ptr(elemAddrPtr), *(int*)elemAddrPtr, key);
+
+    Student studentArr[] = {
+        Student(4, strdup("Sam")),
+        Student(4.5, strdup("Dionys")),
+        Student(4.75, strdup("Severin")),
+        Student(3.5, strdup("Fredy")),
+    };
+
+    for (size_t i = 6 * 4; i >= 1 * 4; i--)
+    {
+        double grade = static_cast<double>(i) / 4;
+        Student* bestStudentPtr = (Student*)linear_search(&grade, &studentArr, 4, sizeof(Student), +[](void* gradePtr, void* studPtr) -> int
+        {
+            double diff = ((Student*)studPtr)->grade - *(double*)gradePtr;
+            if (diff == 0) return 0;
+            return diff < 0 ? -1 : 1;
+        });
+
+        if (bestStudentPtr == nullptr) continue;
+
+        fmt::println(stdout, "The best student is: {}, with grade: {}", bestStudentPtr->namePtr, bestStudentPtr->grade);
+        break;
+    }
+
+    for (auto& i : studentArr)
+    {
+        std::free(i.namePtr);
+    }
     return 0;
 }
